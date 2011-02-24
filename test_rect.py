3ca3c8482387cac456b3f45c0159bae0c622cdc3
Rafael Caricio <rafael.jacinto@gmail.com>
2011-02-23 21:07:16 -0300

    Atualizando os testes de rect.

--- test_rect.py
+++ test_rect.py
@@ -4,18 +4,18 @@ def test_rect_percentages():
     rect = BoundingRect(width=640, height=480)
     rect.set_size(width=320, height=300)
 
-    assert rect.left == 0.0, rect.left
-    assert rect.right == 1.0, rect.right
+    assert rect.left == 0.25, rect.left
+    assert rect.right == 0.75, rect.right
 
-    assert rect.top == 0.03125, rect.top
-    assert rect.bottom == 0.96875, rect.bottom
+    assert rect.top == 0.1875, rect.top
+    assert rect.bottom == 0.8125, rect.bottom
 
 def test_rect_percentages_portrait():
     rect = BoundingRect(width=480, height=640)
     rect.set_size(width=300, height=320)
 
-    assert rect.left == 0.03125, rect.left
-    assert rect.right == 0.96875, rect.right
+    assert rect.left == 0.1875, rect.left
+    assert rect.right == 0.8125, rect.right
 
-    assert rect.top == 0.0, rect.top
-    assert rect.bottom == 1.0, rect.bottom
+    assert rect.top == 0.25, rect.top
+    assert rect.bottom == 0.75, rect.bottom
